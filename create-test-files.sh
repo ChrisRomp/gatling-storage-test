3c48529b0beb8d4a8c63e485ae03f334b0546c41
Chris Romp <chrisromp@gmail.com>
2022-03-14 15:54:47 +0000

    Add random file generator script

---
+++ create-test-files.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+declare filecount=200 # how many of each file
+declare filesizes=('1MB' '2MB' '3MB' '4MB', '5MB', '10MB', '20MB', '50MB', '100MB') # array of file sizes to generate
+declare filelist='./files/fileList.csv' # CSV file list
+
+make_files () {
+    echo "Creating $1 files of $2..."
+
+    # Trim "B" from "MB" for head command, e.g. 1MB -> 1M
+    size=${2::-1}
+
+    for (( i=0; i<$1; i++ ))
+    do
+        head -c $size </dev/urandom >./files/$2.$i
+        echo "$2,/$2.$i" >> $filelist
+    done
+}
+
+# Clear existing files
+echo "Removing previous files..."
+rm ./files/*
+
+# Create file list
+echo "Creating file list..."
+echo "name,uri" > $filelist
+
+# Loop through arrays to create files
+for filesize in "${filesizes[@]}"
+do
+    make_files $filecount $filesize
+done
